f84a8a449ce644ab3a0a6085d93f19d16bc256e3
Werner Luiz Gottschalt <werner@lumiun.com>
2021-07-07 08:34:44 -0300

    Finalização da aula 04

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,69 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="px-24">
+        <h1 class="text-4xl text-center">{{ title }}</h1>
+        <ul role="list" class="mt-5 flex flex-row flex-wrap justify-center items-stretch place-content-around">
+            <li v-for="picture in pictures" :key="picture.url" class="relative mb-10 mx-4">
+                <div class="group block w-72 aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-300 overflow-hidden">
+                    <img :src="picture.url" alt="" class="object-cover pointer-events-none group-hover:opacity-75" />
+                    <button type="button" class="absolute inset-0 focus:outline-none">
+                        <span class="sr-only">View details for {{ picture.title }}</span>
+                    </button>
+                </div>
+                <p class="mt-2 block text-sm font-medium text-gray-900 truncate pointer-events-none">{{ picture.title }}</p>
+            </li>
+        </ul>
+
+        <wl-modal></wl-modal>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+    import WlModal from './components/shared/Modal';
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+    export default {
+
+        components: {
+            WlModal,
+        },
+
+        data () {
+
+            return {
+
+                title: "Coleção de Imagens",
+                pictures: [],
+            }
+        },
+
+        created() {
+
+            this.axios.get("http://localhost:3000/v1/pictures")
+                .then(res => {
+                    this.pictures = res.data
+                })
+                .catch(err => {
+                    if (err.response) {
+                        // The request was made and the server responded with a status code
+                        // that falls out of the range of 2xx
+                        console.log(err.response.data);
+                        console.log(err.response.status);
+                        console.log(err.response.headers);
+                    } else if (err.request) {
+                        // The request was made but no response was received
+                        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                        // http.ClientRequest in node.js
+                        console.log('Request:');
+                        console.log(err.request);
+                    } else {
+                        // Something happened in setting up the request that triggered an Error
+                        console.log('Error', err.message);
+                    }
+                    console.log('Config:');
+                    console.log(err.config);
+            });
+      
+        }
+
+    }
+
+</script>
---
+++ src/components/shared/panel/ImageCard.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="group block w-72 aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-300 overflow-hidden">
+        <img :src="url" :alt="title" class="object-cover pointer-events-none" />
+    </div>
+    <p class="mt-2 block text-sm font-medium text-gray-900 truncate pointer-events-none">{{ title }}</p>
+</template>
+
+<script>
+    export default {
+
+        props: {
+            url: { type: String, default: '#' },
+            title: { type: String, default: '' }
+        }
+
+    }
+</script>
