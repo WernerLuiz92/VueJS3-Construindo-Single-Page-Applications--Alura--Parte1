fc85c07d05a8655375248fa29398525188e91dd1
Werner Luiz Gottschalt <werner@lumiun.com>
2021-08-06 09:59:15 -0300

    feat: nomeando rotas feat: adicionado baseUrl para API no main refactor: implementado exclusão de forma mais concisa.

--- src/main.js
+++ src/main.js
@@ -9,7 +9,11 @@ import './assets/css/tailwind.css';
 
 const app = createApp(App);
 
-app.use(VueAxios, axios);
+app.use(
+    VueAxios, 
+    axios, 
+    axios.defaults.baseURL = 'http://localhost:3000/'
+);
 app.use(router);
 app.directive('wl-transform',Transform)
 app.mount('#app');
